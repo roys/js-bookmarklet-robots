99e450917f8a700f93dbd450293854fb61e03d02
Roy Solberg <git@roysolberg.com>
2018-06-04 18:06:35 +0200

    Initial import of version 1.0.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# js-bookmarklet-robots
-robots.txt linkifier
+# robots.txt linkifier
+See https://blog.roysolberg.com/2018/06/robots-txt-linkifier for more details. :)
---
+++ bookmarklet.js
@@ -0,0 +1,29 @@
+/*
+* ----------------------------------------------------------------------------
+* "THE BEER-WARE LICENSE" (Revision 42):
+* http://github.com/roys wrote this file.  As long as you retain this notice
+* you can do whatever you want with this stuff. If we meet some day, and you
+* think this stuff is worth it, you can buy me a beer in return.   Roy Solberg
+* ----------------------------------------------------------------------------
+*/
+javascript: (function () {
+    console.log('robots.txt linkifier v1.0; https://blog.roysolberg.com');
+    function openLinks() {
+        var links = document.links;
+        if (links.length > 20) {
+            if (!confirm('There are ' + links.length + ' links. Are you sure you want to open them all at once?')) {
+                return;
+            }
+        }
+        console.log('Some browsers will block opening links this way.');
+        for (var i = 0; i < links.length; i++) {
+            window.open(links[i].href, '_blank');
+        };
+    }
+    var base = location.protocol + "//" + location.hostname + (location.port && ":" + location.port);
+    var html = '<body style="font-size:120%;"><script>' + openLinks.toString() + '</script><button type="button" style="width:200px;height:40px;font-size:120%;" onclick="openLinks();">Open all links</button><div style="font-family: monospace;">';
+    html += document.body.textContent.replace(/(Allow|Disallow): (\/\S*)/g, '$1: <a href="' + base + '$2" target="_blank">$2</a>').replace(/\n/g, '<br/>');
+    var win = window.open();
+    win.document.write(html);
+    win.document.close();
+})();
